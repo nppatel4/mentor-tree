6acd96d2717c01202c63e667d9da8cccea4a8623
Brian Kim <bkim@solstice.com>
2018-07-24 17:18:53 -0500

    mostly minor formatting fixes * removed unnecessary @EnableTransactionManagement from application * externalized feign config * moved Employee model into its own package * migrated properties to application.yml

--- src/main/java/com/solstice/mentorandtree/mentortree/client/EmployeeClient.java
+++ src/main/java/com/solstice/mentorandtree/mentortree/client/EmployeeClient.java
@@ -1,14 +1,17 @@
 package com.solstice.mentorandtree.mentortree.client;
 
-import com.solstice.mentorandtree.mentortree.Employee;
+import com.solstice.mentorandtree.mentortree.model.Employee;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 @Component
-@FeignClient(name = "employees", url = "http://localHost:8080")
+@FeignClient(name = "${feign.employee-client.name}", url = "${feign.employee-client.url}")
 public interface EmployeeClient {
+
     @RequestMapping(method = RequestMethod.GET, value = "/employees/{id}")
-    Employee  getEmployeeById(@PathVariable("id") Long id);
+    Employee getEmployeeById(@PathVariable("id") Long id);
+
 }
--- src/main/java/com/solstice/mentorandtree/mentortree/repository/MentorTreeRepository.java
+++ src/main/java/com/solstice/mentorandtree/mentortree/repository/MentorTreeRepository.java
@@ -9,13 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface MentorTreeRepository extends Repository<MentorTree, Long> {
+
     List<MentorTree> findByMentorId(Long id);
+
     List<MentorTree> findByTreeLeadId(Long id);
 
     @Transactional
     @Modifying
     @Query("update MentorTree mt set mt.mentorId = ?1, mt.treeLeadId = ?2 where mt.employeeId = ?3")
     void setMentorTreeById(Long mentorId, Long treeLeadId, Long employeeId);
+
     @Transactional
     void deleteByEmployeeId(Long id);
 
